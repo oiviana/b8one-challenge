d3c08cc4e35ea4ca67463aae8440d55fed387290
decobot <capy@deco.cx>
2024-10-08 14:57:20 -0300

    fix: config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,21 +6,19 @@ export default {
   ],
   theme: {
     extend: {
-      extend: {
-        colors: {
-          'c-green-100': '#A3F9B9',
-          'c-green-400': '#40B25C',
-          'c-green-600': '#1C6C3E',
-          'c-red-500': '#DA4B4F',
-          'c-red-600': '#C22539',
-          'c-pink-100': '#FEE5EC',
-          'c-grayscale-0': '##FFFFFF',
-          'c-grayscale-200': '#F2F3F6',
-          'c-grayscale-600': '#B5B5B6',
-          'c-grayscale-900': '#1C1C1C',
-        },
+      colors: {
+        'c-green-100': '#A3F9B9',
+        'c-green-400': '#40B25C',
+        'c-green-600': '#1C6C3E',
+        'c-red-500': '#DA4B4F',
+        'c-red-600': '#C22539',
+        'c-pink-100': '#FEE5EC',
+        'c-grayscale-0': '#FFFFFF',
+        'c-grayscale-200': '#F2F3F6',
+        'c-grayscale-600': '#B5B5B6',
+        'c-grayscale-900': '#1C1C1C',
       },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
 }
