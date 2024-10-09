6ee8af25fc103d5da92c74e3b36ba71fe81930a4
oiviana <vianalucas80@gmail.com>
2024-10-08 22:24:54 -0300

    feat: wishlist button boolean

--- src/components/Wishlist/WishlistButton/index.tsx
+++ src/components/Wishlist/WishlistButton/index.tsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
 import { FiHeart } from "react-icons/fi";
 
 export default function WishlistButton() {
 
+    const [selected, setSelected] = useState(false);
+
+    const handleSelectProduct = () =>{
+        setSelected(!selected)
+    }
+
     return (
-        <button className="w-12 h-12 rounded-full bg-c-grayscale-200 text-c-grayscale-900 absolute top-0 right-0 flex justify-center items-center hover:bg-c-pink-100  hover:text-c-red-500 transition-colors">
-            <FiHeart size={25} className="mt-[2px]"/>
+        <button className={`w-12 h-12 rounded-full bg-c-grayscale-200 text-c-grayscale-900 absolute top-0 right-0 flex justify-center items-center hover:bg-c-pink-100  hover:text-c-red-500 transition-colors ${selected && 'bg-c-red-500  hover:bg-c-red-600'}`}
+        onClick={handleSelectProduct}>
+            <FiHeart size={25} className={`mt-[2px] ${selected && 'text-c-grayscale-0 fill-c-grayscale-0'}`}/>
         </button>
     );
 }
