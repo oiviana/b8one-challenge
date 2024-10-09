b2c6e89873f29547a43d496c7e0eddfe2b52ef21
oiviana <vianalucas80@gmail.com>
2024-10-08 21:00:33 -0300

    feat: mocked content

--- src/components/Product/mock.ts
+++ src/components/Product/mock.ts
@@ -1,21 +1,31 @@
-export const sliderMock: ProductData[] = [
+export const productMock: ProductData[] = [
     {
         productId: "e34faf41-2459-494f-87db-f0f47dd86501",
         productName: "Monitor LED 27'' Gamer Curvo Samsung  1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50",
         productImage: "/mock/product1.png",
         productPrice: {
-            listPrice: 2814,
-            sellingPrice: 2599,
+            listPrice: 2813.99,
+            sellingPrice: 2600,
             installments: 10
         }
     },
     {
-        productId: "e34faf41-2459-494f-87db-f0f47dd86501",
-        productName: "Monitor LED 27'' Gamer Curvo Samsung  1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50",
-        productImage: "/mock/product1.png",
+        productId: "623beee9-8ba6-4f00-855e-12f720e0a55d",
+        productName: "Placa de Vídeo RTX 3060 Ventus 2X MSI NVIDIA GeForce, 12GB GDDR6, DLSS, Ray Tracing - RTX 3060 Ventus 2X 12G OC",
+        productImage: "/mock/product2.png",
         productPrice: {
-            listPrice: 2814,
-            sellingPrice: 2599,
+            listPrice: 2115.28,
+            sellingPrice: 1880,
+            installments: 6
+        }
+    },
+    {
+        productId: "c98c2414-e87a-402b-963a-67d8674c685b",
+        productName: "Caixa de Som Portátil JBL Partybox 110, LED, 160W RMS, Bluetooth, Á Prova de Respingos, Preto",
+        productImage: "/mock/product3.png",
+        productPrice: {
+            listPrice: 2899.99,
+            sellingPrice: 2100,
             installments: 10
         }
     }
