e160c44f10bf0ab67b19780996365033f6e2506f
oiviana <vianalucas80@gmail.com>
2024-10-08 22:25:09 -0300

    style: prettier

--- src/components/Wishlist/WishlistButton/index.tsx
+++ src/components/Wishlist/WishlistButton/index.tsx
@@ -5,14 +5,14 @@ export default function WishlistButton() {
 
     const [selected, setSelected] = useState(false);
 
-    const handleSelectProduct = () =>{
+    const handleSelectProduct = () => {
         setSelected(!selected)
     }
 
     return (
         <button className={`w-12 h-12 rounded-full bg-c-grayscale-200 text-c-grayscale-900 absolute top-0 right-0 flex justify-center items-center hover:bg-c-pink-100  hover:text-c-red-500 transition-colors ${selected && 'bg-c-red-500  hover:bg-c-red-600'}`}
-        onClick={handleSelectProduct}>
-            <FiHeart size={25} className={`mt-[2px] ${selected && 'text-c-grayscale-0 fill-c-grayscale-0'}`}/>
+            onClick={handleSelectProduct}>
+            <FiHeart size={25} className={`mt-[2px] ${selected && 'text-c-grayscale-0 fill-c-grayscale-0'}`} />
         </button>
     );
 }
