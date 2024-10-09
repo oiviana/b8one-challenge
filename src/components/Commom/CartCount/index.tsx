8477f60318aea74843bca8352d311651d5897cac
oiviana <vianalucas80@gmail.com>
2024-10-09 14:47:23 -0300

    refactor: cartcount display

--- src/components/Commom/CartCount/index.tsx
+++ src/components/Commom/CartCount/index.tsx
@@ -1,12 +1,13 @@
 type CartCountProps = {
     quantity: number;
-}
+};
 
-export default function CartCount({quantity}:CartCountProps) {
-
+export default function CartCount({ quantity }: CartCountProps) {
     return (
-        <span className="bg-c-grayscale-900 rounded-full w-5 h-5 text-sm text-c-grayscale-0 absolute top-[10px] right-0 flex items-center justify-center font-semibold">
-            {quantity}
-        </span>
+        quantity > 0 && (
+            <span className="bg-c-grayscale-900 rounded-full w-5 h-5 text-sm text-c-grayscale-0 absolute top-[10px] right-0 flex items-center justify-center font-semibold">
+                {quantity}
+            </span>
+        )
     );
 }
