de086aea643bf82999a7fb3cacf879a8752422c1
oiviana <vianalucas80@gmail.com>
2024-10-08 21:00:56 -0300

    feat: product grid and product card props

--- src/components/Product/ProductCard/index.tsx
+++ src/components/Product/ProductCard/index.tsx
@@ -1,22 +1,25 @@
 import AddToCart from "../../Cart/AddToCart";
 import WishlistButton from "../../Wishlist/WishlistButton";
 
-export default function ProductCard() {
+export default function ProductCard(ProductData: ProductData) {
+    const {productName, productImage, productPrice} = ProductData
+    const {sellingPrice, listPrice, installments} = productPrice 
+    const parceledPrice = sellingPrice / installments
 
     return (
         <div className="bg-c-grayscale-0 w-full max-w-[304px] h-[568px] p-7 flex flex-col rounded-lg shadow-md mx-auto">
             <div className="relative w-full h-auto max-h-60  max-w-60 overflow-hidden ">
-                <img src="/mock/product1.png" alt="teste" />
+                <img src={productImage} alt="teste" />
                 <WishlistButton/>
             </div>
             <div className="flex flex-col self-end items-start gap-6 flex-1  justify-between">
                 <span className="text-sm text-c-grayscale-900 font-medium mt-4 overflow-hidden text-ellipsis line-clamp-[4] max-h-20">
-                    Monitor LED 27'' Gamer Curvo Samsung  1920 x 1080 FHD 240 Hz HDMI, DP, Gsync Série CRG50
+                    {productName}
                 </span>
                 <div className="flex flex-col gap-1">
-                    <span className="text-sm text-c-grayscale-600 line-through">R$ 2.813,99</span>
-                    <span className="text-xl font-semibold text-c-red-500">R$ 2.599,00</span>
-                    <span className="text-xs text-c-grayscale-900">em até<b> 10x de R$ 259,90 </b>sem juros</span>
+                    <span className="text-sm text-c-grayscale-600 line-through">R$ {listPrice}</span>
+                    <span className="text-xl font-semibold text-c-red-500">R$ {sellingPrice}</span>
+                    <span className="text-xs text-c-grayscale-900">em até<b> {installments}x de R$ {parceledPrice}</b>sem juros</span>
                 </div>
                 <AddToCart />
             </div>
--- src/components/Product/ProductGrid/index.tsx
+++ src/components/Product/ProductGrid/index.tsx
@@ -1,13 +1,22 @@
 import ProductCard from "../ProductCard";
+import { productMock } from "../mock";
 
+const products = productMock.map((product) =>{
+    return(
+        <ProductCard
+        productId={product.productId}
+        productImage={product.productImage}
+        productName={product.productName}
+        productPrice={product.productPrice}
+        key={product.productId}
+        />
+    )
+})
 export default function ProductGrid(){
 
     return(
         <div className="grid grid-cols-1 gap-6 xl:gap-0 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-[1816px] mx-auto">
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
-            <ProductCard/>
+          {products}
         </div>
     )
 }
