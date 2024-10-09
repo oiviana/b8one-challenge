88c5a2cb471de9838541acb6236c6ee4552d4dc3
oiviana <vianalucas80@gmail.com>
2024-10-09 15:15:26 -0300

    style: fixing add to cart bg

--- src/components/Cart/AddToCart/index.tsx
+++ src/components/Cart/AddToCart/index.tsx
@@ -17,7 +17,7 @@ export default function AddToCart({productId}:AddToCartProps) {
     }
 
     return (
-        <button className={`w-full h-12 flex justify-center gap-3 items-center bg-c-green-400 rounded text-c-grayscale-0 text-base font-bold uppercase hover:bg-c-green-600 transition-colors ${selected && 'bg-c-green-100 text-c-grayscale-900 hover:bg-c-green-400 transition-colors'}`}
+        <button className={`w-full h-12 flex justify-center gap-3 items-center bg-c-green-400 rounded text-c-grayscale-0 text-base font-bold uppercase hover:bg-c-green-600 transition-colors ${selected && '!bg-c-green-100 text-c-grayscale-900 transition-colors'}`}
             onClick={() => handleAddProduct(productId)}>
             {selected ? (
                 <>
