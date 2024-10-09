03516d54059503520c5119fd62d77246a8a6bc41
oiviana <vianalucas80@gmail.com>
2024-10-09 14:46:57 -0300

    feat: adding toggle item function

--- src/components/Cart/AddToCart/index.tsx
+++ src/components/Cart/AddToCart/index.tsx
@@ -1,15 +1,24 @@
 import { useState } from "react";
 import { FaCheck } from "react-icons/fa6";
+import { useProduct } from "../../../context/ProductContext";
 
-export default function AddToCart() {
+type AddToCartProps = {
+    productId: string;
+}
+
+export default function AddToCart({productId}:AddToCartProps) {
+
     const [selected, setSelected] = useState(false);
-    const handleAddProduct = () => {
+    const { toggleMiniCartItem } = useProduct();
+
+    const handleAddProduct = (id:string) => {
         setSelected(!selected)
+        toggleMiniCartItem(id)
     }
 
     return (
         <button className={`w-full h-12 flex justify-center gap-3 items-center bg-c-green-400 rounded text-c-grayscale-0 text-base font-bold uppercase hover:bg-c-green-600 transition-colors ${selected && 'bg-c-green-100 text-c-grayscale-900 hover:bg-c-green-400 transition-colors'}`}
-            onClick={handleAddProduct}>
+            onClick={() => handleAddProduct(productId)}>
             {selected ? (
                 <>
                     <FaCheck size={20} />
--- src/components/Cart/Minicart/index.tsx
+++ src/components/Cart/Minicart/index.tsx
@@ -1,11 +1,12 @@
 import { RiShoppingBag4Line } from "react-icons/ri";
 import CartCount from "../../Commom/CartCount";
+import { useProduct } from "../../../context/ProductContext";
 
 export default function MiniCart() {
-
+const {miniCartQuantity} = useProduct();
     return (
         <button className="text-c-grayscale-900 relative">
-            <CartCount quantity={2}/>
+            <CartCount quantity={miniCartQuantity}/>
             <RiShoppingBag4Line
                 size={40} />
         </button>
